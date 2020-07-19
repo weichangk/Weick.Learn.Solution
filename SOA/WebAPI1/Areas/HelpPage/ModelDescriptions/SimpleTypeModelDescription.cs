da45fe877b36263286c8575077156851ee891b98
weichangk <weichangk@qq.com>
2020-07-20 00:07:57 +0800

    deleted WebAPI1

--- SOA/WebAPI1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebAPI1.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- SOA/WebAPI1/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPI1.WebApiApplication" Language="C#" %>
