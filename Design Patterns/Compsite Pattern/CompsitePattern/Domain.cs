ac651089d1e5e1011b4698afa99c49bd47bd7655
weichangk <weichangk@qq.com>
2020-10-11 23:42:47 +0800

    add CompsitePattern

---
+++ Design Patterns/Compsite Pattern/CompsitePattern/Domain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompsitePattern
+{
+    /// <summary>
+    /// 机构/个人
+    /// </summary>
+    public class Domain : AbstractDomain
+    {
+        //组合模式（Composite Pattern） 也称为 整体-部分（Part-Whole）模式，它的宗旨是通过将单个对象（叶子节点）和组合对象（树枝节点）用相同的接口进行表示，使得客户对单个对象和组合对象的使用具有一致性。
+        //组合模式 一般用来描述 整体 与 部分 的关系，它将对象组织到树形结构中，最顶层的节点称为 根节点，根节点下面可以包含 树枝节点 和 叶子节点，树枝节点下面又可以包含 树枝节点 和 叶子节点。
+        //树枝内部组合该接口，并且含有内部属性 List，里面放相同对象组成树形结构。
+        private List<Domain> DomainChildList = new List<Domain>();
+
+
+        //组合模式 核心：借助同一接口，使叶子节点和树枝节点的操作具备一致性。
+        public void AddChild(Domain domainChild)
+        {
+            this.DomainChildList.Add(domainChild);
+        }
+
+
+        //递归组合模式树形结构
+        public override void Commission(double total)
+        {
+            double result = total * this.Percent / 100;
+            Console.WriteLine("this {0} 提成 {1}", this.Name, result);
+
+            foreach (var domainChild in DomainChildList)
+            {
+                domainChild.Commission(result);
+            }
+
+        }
+
+    }
+}
---
+++ Design Patterns/Compsite Pattern/CompsitePattern/Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompsitePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //递归获取文件夹个数
+                //var list = Recursion.GetDirectoryList(@"E:\practice\Weick.Learn.Solution");
+                //Console.WriteLine(list.Count());
+
+
+                //利用组合模式计算公司员工提成
+                double total = 1000000;
+                Domain domain = BuildTree();
+
+                domain.Commission(total);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
+        }
+
+
+        //建立组合模式树形结构对象
+        private static Domain BuildTree()
+        {
+
+            Domain domain = new Domain()
+            {
+                Name = "能有的提成收入",
+                Percent = 10
+            };
+
+            #region
+            Domain domain1 = new Domain()
+            {
+                Name = "CEO",
+                Percent = 30
+            };
+            Domain domain2 = new Domain()
+            {
+                Name = "各部门共有",
+                Percent = 70
+            };
+
+            Domain domain21 = new Domain()
+            {
+                Name = "实施",
+                Percent = 20
+            };
+            Domain domain22 = new Domain()
+            {
+                Name = "测试",
+                Percent = 10
+            };
+            Domain domain23 = new Domain()
+            {
+                Name = "销售",
+                Percent = 30
+            };
+            Domain domain24 = new Domain()
+            {
+                Name = "开发",
+                Percent = 40
+            };
+            Domain domain241 = new Domain()
+            {
+                Name = "经理",
+                Percent = 20
+            };
+            Domain domain242 = new Domain()
+            {
+                Name = "主管",
+                Percent = 15
+            };
+            Domain domain243 = new Domain()
+            {
+                Name = "开发团队",
+                Percent = 65
+            };
+            Domain domain2431 = new Domain()
+            {
+                Name = "项目组1",
+                Percent = 50
+            };
+            Domain domain2432 = new Domain()
+            {
+                Name = "项目组2",
+                Percent = 50
+            };
+            Domain domain24321 = new Domain()
+            {
+                Name = "项目经理",
+                Percent = 20
+            };
+            Domain domain24322 = new Domain()
+            {
+                Name = "开发人员",
+                Percent = 80
+            };
+            Domain domain243221 = new Domain()
+            {
+                Name = "高级开发人员",
+                Percent = 40
+            };
+            Domain domain243222 = new Domain()
+            {
+                Name = "中级开发人员",
+                Percent = 30
+            };
+            Domain domain243223 = new Domain()
+            {
+                Name = "初级开发人员",
+                Percent = 20
+            };
+            Domain domain243224 = new Domain()
+            {
+                Name = "实习生",
+                Percent = 10
+            };
+            Domain domain2432241 = new Domain()
+            {
+                Name = "实习生1",
+                Percent = 25
+            };
+            Domain domain2432242 = new Domain()
+            {
+                Name = "实习生2",
+                Percent = 25
+            };
+            Domain domain2432243 = new Domain()
+            {
+                Name = "实习生3",
+                Percent = 25
+            };
+            Domain domain2432244 = new Domain()
+            {
+                Name = "实习生4",
+                Percent = 25
+            };
+            domain243224.AddChild(domain2432241);
+            domain243224.AddChild(domain2432242);
+            domain243224.AddChild(domain2432243);
+            domain243224.AddChild(domain2432244);
+
+            domain24322.AddChild(domain243221);
+            domain24322.AddChild(domain243222);
+            domain24322.AddChild(domain243223);
+            domain24322.AddChild(domain243224);
+
+            domain2432.AddChild(domain24321);
+            domain2432.AddChild(domain24322);
+
+            domain243.AddChild(domain2431);
+            domain243.AddChild(domain2432);
+
+            domain24.AddChild(domain241);
+            domain24.AddChild(domain242);
+            domain24.AddChild(domain243);
+
+            domain2.AddChild(domain21);
+            domain2.AddChild(domain22);
+            domain2.AddChild(domain23);
+            domain2.AddChild(domain24);
+
+            domain.AddChild(domain1);
+            domain.AddChild(domain2);
+            #endregion
+
+            return domain;
+        }
+    }
+}
